5a9d73a4d43a38bfd5486b2eb36a51c7915d3391
tylim <limteikyee@gmail.com>
2023-05-01 04:31:49 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -15,20 +15,29 @@ const port = env.port
 // https://itnext.io/node-express-letsencrypt-generate-a-free-ssl-certificate-and-run-an-https-server-in-5-minutes-a730fbe528ca
 
 if (env.production) {
+    const privateKey = fs.readFileSync(
+        '/etc/letsencrypt/live/yourdomain.com/privkey.pem',
+        'utf8'
+    )
+    const certificate = fs.readFileSync(
+        '/etc/letsencrypt/live/yourdomain.com/cert.pem',
+        'utf8'
+    )
+    const ca = fs.readFileSync(
+        '/etc/letsencrypt/live/yourdomain.com/chain.pem',
+        'utf8'
+    )
+
+    const credentials = {
+        key: privateKey,
+        cert: certificate,
+        ca: ca,
+    }
     app.use(express.static(__dirname, { dotfiles: 'allow' }))
-    https
-        .createServer(
-            {
-                key: fs.readFileSync('../key.pem'),
-                cert: fs.readFileSync('../cert.pem'),
-            },
-            app
-        )
-        .listen(port, () => {
-            console.info(`(https)Listening at port ${port}`)
-        })
-} else {
-    http.createServer(app).listen(port, () => {
-        console.info(`(http)Listening at port ${port}`)
+    https.createServer(credentials, app).listen(port, () => {
+        console.info(`(https)Listening at port ${port}`)
     })
 }
+http.createServer(app).listen(port, () => {
+    console.info(`(http)Listening at port ${port}`)
+})
