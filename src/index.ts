3dd2c97784551d238b9f2d63ad489b328c9d1e35
tylim <limteikyee@gmail.com>
2023-05-01 00:04:46 +0800

    remove some script

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,24 @@ import { contracts } from './contracts'
 import { app } from './server'
 import { createExpressEndpoints } from '@ts-rest/express'
 import { createTranscriptionRouter } from './router'
+import https from 'https'
+import env from '../env.json'
+import fs from 'fs'
 
 createExpressEndpoints(contracts, createTranscriptionRouter, app)
 
-const port = process.env.port || 3001
-app.listen(port, () => {
-    console.log(`Listening at http://localhost:${port}`)
+const options: { key?: Buffer; cert?: Buffer } = {}
+
+// https://adamtheautomator.com/https-nodejs/
+try {
+    options.key = fs.readFileSync('../key.pem')
+    options.cert = fs.readFileSync('../cert.pem')
+} catch (e) {
+    if (env.production) {
+        throw Error('ssl cert or key not found')
+    }
+}
+const port = env.port
+https.createServer(options, app).listen(port, () => {
+    console.info(`Listening at port ${port}`)
 })
