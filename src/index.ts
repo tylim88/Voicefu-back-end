be408a3dc07685d01b3fea546474a3bff36af540
tylim <limteikyee@gmail.com>
2023-05-01 12:52:04 +0800

    add greenlock for ssl

--- src/index.ts
+++ src/index.ts
@@ -2,42 +2,30 @@ import { contracts } from './contracts'
 import { app } from './server'
 import { createExpressEndpoints } from '@ts-rest/express'
 import { createTranscriptionRouter } from './router'
-import https from 'https'
 import env from '../env.json'
-import fs from 'fs'
 import http from 'http'
-import express from 'express'
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const greenlock = require('greenlock-express')
 
 createExpressEndpoints(contracts, createTranscriptionRouter, app)
 
-const port = env.port
-
-// https://itnext.io/node-express-letsencrypt-generate-a-free-ssl-certificate-and-run-an-https-server-in-5-minutes-a730fbe528ca
-
 if (env.production) {
-    const privateKey = fs.readFileSync(
-        '/etc/letsencrypt/live/yourdomain.com/privkey.pem',
-        'utf8'
-    )
-    const certificate = fs.readFileSync(
-        '/etc/letsencrypt/live/yourdomain.com/cert.pem',
-        'utf8'
-    )
-    const ca = fs.readFileSync(
-        '/etc/letsencrypt/live/yourdomain.com/chain.pem',
-        'utf8'
-    )
+    greenlock
+        .init({
+            packageRoot: __dirname,
+            configDir: '../greenlock',
 
-    const credentials = {
-        key: privateKey,
-        cert: certificate,
-        ca: ca,
-    }
-    app.use(express.static(__dirname, { dotfiles: 'allow' }))
-    https.createServer(credentials, app).listen(port, () => {
-        console.info(`(https)Listening at port ${port}`)
+            // contact for security and critical bug notices
+            maintainerEmail: env.email,
+
+            // whether or not to run at cloudscale
+            cluster: false,
+        })
+        // Serves on 80 and 443
+        // Get's SSL certificates magically!
+        .serve(app)
+} else {
+    http.createServer(app).listen(3001, () => {
+        console.info(`(http)Listening at port 3001`)
     })
 }
-http.createServer(app).listen(port, () => {
-    console.info(`(http)Listening at port ${port}`)
-})
