59fb5bd23ba654111ebe791e8d656964dd9d6690
tylim <limteikyee@gmail.com>
2024-09-01 23:23:46 +0800

    update

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,16 @@ import cors from 'cors'
 import env from '../env.json'
 
 export const app = express()
-
-app.use(cors({ origin: env.cors }))
+const corsOptions = {
+    origin: function (origin: string, callback: (...args: unknown[]) => void) {
+        if (env.cors.indexOf('*') !== -1 || env.cors.indexOf(origin) !== -1) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'))
+        }
+    },
+}
+// @ts-expect-error 123
+app.use(cors(corsOptions))
 
 export const s = initServer()
